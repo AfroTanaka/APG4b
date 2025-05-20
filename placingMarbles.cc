5bb473d63d3d70a155a3e10ae128e5b922066f59
AfroTanaka <heapoverfl0win@icloud.com>
2025-05-20 16:59:38 +0900

    Done til Section F on APG4b

---
+++ placingMarbles.cc
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int res;
+    cin >> res;
+    cout << res % 9 << endl;
+}
---
+++ ratingGoal.cc
@@ -0,0 +1,12 @@
+// (R + b) / 2 = G
+// R + b = 2G
+// b = 2G - R
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int r, g;
+    cin >> r >> g;
+    cout << 2 * g - r;
+}
